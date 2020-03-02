37e84eb45e053b7460ce717187035ba521371aa6
“zhongzhiguo <1245619686@qq.com>
2020-03-02 17:11:53 +0800

    第一次提交

---
+++ Assets/script/firstScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstScript : MonoBehaviour {
+
+    public GameObject prefabCube;
+    bool GetCube = true;
+	void Start () {
+       GameObject go1= Instantiate(prefabCube, Vector3.zero, Quaternion.identity);
+        go1.AddComponent<Rigidbody>();
+        StartCoroutine(BronCube());
+	}
+    public IEnumerator BronCube()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1);
+           GameObject go= Instantiate(prefabCube, Vector3.zero, Quaternion.identity);
+            go.AddComponent<Rigidbody>();
+        }
+    }
+	 
+	void Update () {
+		
+	}
+}
