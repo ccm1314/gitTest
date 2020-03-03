b9f4da83793c228dc122238e5b6e852b0034d17c
“zhongzhiguo <1245619686@qq.com>
2020-03-03 16:21:39 +0800

    第二次修改thridScript.cs

---
+++ Assets/script/secondeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("第一次修改");
+        Debug.Log("aa");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/script/thridScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("第2次修改");
+         
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
